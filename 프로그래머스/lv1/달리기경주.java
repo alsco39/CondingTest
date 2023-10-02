62c468aecda3519e62570e07d8cdb81ea98ec305
김민채 <103401813+alsco39@users.noreply.github.com>
2023-10-02 22:04:25 +0900

    ♻️ ::

--- 프로그래머스/lv1/달리기경주.java
+++ 프로그래머스/lv1/달리기경주.java
@@ -1,5 +1,6 @@
 class Solution {
     public String[] solution(String[] players, String[] callings) {
+        //TODO for문은 타임아웃. HashMap으로 변경 해야함
         
         for(String call : callings) {
             for(int i = 0; i < players.length; i++) {
